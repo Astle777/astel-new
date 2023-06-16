2666e430fd321c493e556a1275fc8803e0bc2617
Astle777 <129484939+Astle777@users.noreply.github.com>
2023-06-16 13:08:06 +0530

    Add files via upload

--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[STATIC_DIR,]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS=[STATIC_DIR,]
